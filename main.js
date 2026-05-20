326c38b3301f0c252251bf6f72f7f2b127c6b8d3
richardvinc <vincrichard@live.com>
2026-05-20 20:35:28 +0700

    adjust copyright text

--- main.js
+++ main.js
@@ -403,7 +403,7 @@ let copyrightText;
 async function initCopyrightText() {
   await loadFonts();
 
-  copyrightText = createTextPlane(`Project Lemonade by Manoosia.id`);
+  copyrightText = createTextPlane(`Project Lemonade`);
   scene.add(copyrightText);
 
   positionCopyrightText();
